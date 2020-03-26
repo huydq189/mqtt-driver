05e10f7f76f073d11ebd74f60a3b356fd945ec8c
Đỗ Quốc Huy <doquochuy189@gmail.com>
2020-03-26 17:09:35 +0700

    push  version 1

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"time"
+
+	MQTT "bitbucket.org/mqttgis/transport"
+)
+
+var knt int
+
+// LatLng - struct
+type LatLng struct {
+	LAT float64 `json:"lat"`
+	LON float64 `json:"lon"`
+}
+
+func main() {
+	// knt = 0
+	// c := make(chan os.Signal, 1)
+	// signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	client := MQTT.CreateMQTTClient()
+	client.Start()
+	// opts := mqtt.NewClientOptions().AddBroker("tcp://mqtt.dioty.co:1883")
+	// opts.SetClientID("phuc123")
+	// opts.SetUsername("salt.coffee189@gmail.com")
+	// opts.SetPassword("bf64fe27")
+	// opts.SetDefaultPublishHandler(f)
+	// topic := "phuc-topic"
+
+	// opts.OnConnect = func(c mqtt.Client) {
+	// 	if token := c.Subscribe(topic, 0, f); token.Wait() && token.Error() != nil {
+	// 		panic(token.Error())
+	// 	}
+	// }
+
+	var GPS [10]LatLng
+	// GPS[0] = LatLng{LAT: 10.502746124283656, LON: 107.38368597037162}
+	// GPS[1] = LatLng{LAT: 10.553810837476034, LON: 107.20132184125767}
+	// GPS[2] = LatLng{LAT: 10.548570829582767, LON: 107.17570386943207}
+	// GPS[3] = LatLng{LAT: 10.458918206895515, LON: 107.07248052315317}
+	// GPS[4] = LatLng{LAT: 10.544808701831574, LON: 107.01121969501403}
+	// GPS[5] = LatLng{LAT: 10.4491585074751001, LON: 107.23079953359036}
+	// GPS[6] = LatLng{LAT: 10.468135281158133, LON: 107.00127921464853}
+	// GPS[7] = LatLng{LAT: 10.450887946734518, LON: 107.35589569018214}
+	// GPS[8] = LatLng{LAT: 10.490830999209736, LON: 107.0104126888608}
+	// GPS[9] = LatLng{LAT: 10.472714857173292, LON: 107.24762576346343}
+	GPS[0] = LatLng{LAT: 10.445595, LON: 107.186590}
+	GPS[1] = LatLng{LAT: 10.510079, LON: 107.246316}
+	GPS[2] = LatLng{LAT: 10.512941, LON: 107.276870}
+	GPS[3] = LatLng{LAT: 10.556337, LON: 107.321678}
+	GPS[4] = LatLng{LAT: 10.572473, LON: 107.274554}
+	GPS[5] = LatLng{LAT: 10.562583, LON: 107.231137}
+	GPS[6] = LatLng{LAT: 10.521461, LON: 107.244374}
+	GPS[7] = LatLng{LAT: 10.496993, LON: 107.277607}
+	GPS[8] = LatLng{LAT: 10.474085, LON: 107.248610}
+	GPS[9] = LatLng{LAT: 10.477209, LON: 107.297322}
+
+	// client := mqtt.NewClient(opts)
+	for i := 0; i < len(GPS); i++ {
+		data, _ := json.Marshal(&GPS[i])
+		client.Publish("/salt.coffee189@gmail.com/test", string(data))
+		time.Sleep(1 * time.Second)
+	}
+}
---
+++ transport/mqtt_client.go
@@ -0,0 +1,226 @@
+package transport
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/google/uuid"
+	"github.com/spf13/viper"
+)
+
+var (
+	errStillConnected   = errors.New("mqtt: still connected. Kill all processes manually")
+	errMQTTDisconnected = errors.New("mqtt: disconnected")
+)
+
+// MQTT is the implementation of the MQTT client
+type MQTT struct {
+	host            string
+	port            int
+	prefix          string
+	clientID        string
+	sslEnabled      bool
+	username        string
+	password        string
+	clientCertPath  string
+	privateKeyPath  string
+	keepAliveSec    int
+	pingTimeoutSec  int
+	subscriptionQos byte
+	persistent      bool
+	order           bool
+	connecting      bool
+	disconnected    bool
+	client          mqtt.Client
+	connectToken    *mqtt.ConnectToken
+}
+
+func (m *MQTT) getProtocol() string {
+	if m.sslEnabled == true {
+		return "ssl"
+	}
+	return "tcp"
+}
+
+func initMQTTClientOptions(m *MQTT) (*mqtt.ClientOptions, error) {
+
+	opts := mqtt.NewClientOptions()
+
+	if m.username != "" {
+		opts.SetUsername(m.username)
+	}
+	if m.password != "" {
+		opts.SetPassword(m.password)
+	}
+	opts.AddBroker(fmt.Sprintf("%s://%s:%v", m.getProtocol(), m.host, m.port))
+	opts.SetClientID(m.clientID)
+	opts.SetCleanSession(!m.persistent)
+	opts.SetOrderMatters(m.order)
+	opts.SetKeepAlive(time.Duration(m.keepAliveSec) * time.Second)
+	opts.SetPingTimeout(time.Duration(m.pingTimeoutSec) * time.Second)
+	opts.SetAutoReconnect(false)
+	opts.SetConnectionLostHandler(m.connectionLostHandler)
+	return opts, nil
+}
+
+// CreateMQTTClient creates a new MQTT client
+func CreateMQTTClient() *MQTT {
+	viper.SetConfigFile(`./config/config.json`)
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+	connID := uuid.New().String()
+	mqttClient := &MQTT{
+		host:            viper.GetString(`mqtt.host`),
+		port:            viper.GetInt(`mqtt.port`),
+		prefix:          viper.GetString(`mqtt.prefix`),
+		clientID:        viper.GetString(`mqtt.clientID`) + "-" + connID,
+		subscriptionQos: byte(viper.GetInt(`mqtt.subscriptionQos`)),
+		persistent:      viper.GetBool(`mqtt.persistent`),
+		order:           viper.GetBool(`mqtt.order`),
+		sslEnabled:      viper.GetBool(`mqtt.sslEnabled`),
+		username:        viper.GetString(`mqtt.username`),
+		password:        viper.GetString(`mqtt.password`),
+		clientCertPath:  viper.GetString(`mqtt.clientCertPath`),
+		privateKeyPath:  viper.GetString(`mqtt.privateKeyPath`),
+		keepAliveSec:    viper.GetInt(`mqtt.keepAliveSec`),
+		pingTimeoutSec:  viper.GetInt(`mqtt.pingTimeoutSec`),
+	}
+	opts, err := initMQTTClientOptions(mqttClient)
+	if err != nil {
+		log.Fatalf("unable to configure MQTT client: %s", err)
+	}
+	Client := mqtt.NewClient(opts)
+	mqttClient.client = Client
+
+	return mqttClient
+}
+
+// Start running the MQTT client
+func (m *MQTT) Start() {
+	log.Printf("Starting MQTT client on %s://%s:%v with Persistence:%v, OrderMatters:%v, KeepAlive:%v, PingTimeout:%v, QOS:%v\n",
+		m.getProtocol(), m.host, m.port, m.persistent, m.order, m.keepAliveSec, m.pingTimeoutSec, m.subscriptionQos)
+	m.connect()
+}
+
+func (m *MQTT) connect() {
+	m.connectToken = m.client.Connect().(*mqtt.ConnectToken)
+	if m.connectToken.Wait() && m.connectToken.Error() != nil {
+		if !m.connecting {
+			log.Fatalf("MQTT client %s", m.connectToken.Error())
+			m.retryConnect()
+		}
+	} else {
+		fmt.Println("Connected with broker")
+	}
+}
+
+// Stop the MQTT client
+func (m *MQTT) Stop() error {
+	m.client.Disconnect(500)
+	if m.client.IsConnected() {
+		return errStillConnected
+	}
+	return nil
+}
+
+// Subscribe - subcribe to a topic
+func (m *MQTT) Subscribe(topic string) error {
+	if !m.client.IsConnected() {
+		return errMQTTDisconnected
+	}
+	token := m.client.Subscribe(topic, m.subscriptionQos, func(client mqtt.Client, msg mqtt.Message) {
+		fmt.Printf("* [%s] %s\n", msg.Topic(), string(msg.Payload()))
+	})
+	if token.WaitTimeout(2) && token.Error() != nil {
+		return token.Error()
+	}
+	return nil
+}
+
+// SubscribeQOS - subcribe to a topic with QOS
+func (m *MQTT) SubscribeQOS(topic string, qos *byte) error {
+	if !m.client.IsConnected() {
+		return errMQTTDisconnected
+	}
+	var q = m.subscriptionQos
+	if qos != nil {
+		q = *qos
+	}
+	token := m.client.Subscribe(topic, q, func(client mqtt.Client, msg mqtt.Message) {
+		fmt.Printf("* [%s] %s\n", msg.Topic(), string(msg.Payload()))
+	})
+	if token.WaitTimeout(2) && token.Error() != nil {
+		return token.Error()
+	}
+	return nil
+}
+
+// UnSubscribe - Unsubcribe topic
+func (m *MQTT) UnSubscribe(topic string) error {
+	if !m.client.IsConnected() {
+		return errMQTTDisconnected
+	}
+	token := m.client.Unsubscribe(topic)
+	if token.WaitTimeout(3) && token.Error() != nil {
+		return token.Error()
+	}
+	return nil
+}
+
+// Publish - Send messages to topic
+func (m *MQTT) Publish(topic, message string) error {
+	if !m.client.IsConnected() {
+		return errMQTTDisconnected
+	}
+	var q = m.subscriptionQos
+	// Send - Publish a message to Topic
+	token := m.client.Publish(topic, q, false, message)
+	if token.WaitTimeout(2) && token.Error() != nil {
+		return token.Error()
+	}
+	return nil
+}
+
+// PublishQOS - qos
+func (m *MQTT) PublishQOS(topic, message string, qos *byte) error {
+	if !m.client.IsConnected() {
+		return errMQTTDisconnected
+	}
+	var q = m.subscriptionQos
+	if qos != nil {
+		q = *qos
+	}
+	// Send - Publish a message to Topic
+	token := m.client.Publish(topic, q, false, message)
+	if token.WaitTimeout(2) && token.Error() != nil {
+		return token.Error()
+	}
+	return nil
+}
+
+// RetryConnect - reconnect after time
+func (m *MQTT) retryConnect() {
+	log.Fatal("MQTT client starting reconnect procedure in background")
+	m.connecting = true
+	ticker := time.NewTicker(time.Second * 5)
+	go func() {
+		for range ticker.C {
+			m.client.Connect()
+			if m.client.IsConnected() {
+				ticker.Stop()
+				m.connecting = false
+			}
+		}
+	}()
+}
+
+//call retryConnect
+func (m *MQTT) connectionLostHandler(c mqtt.Client, err error) {
+	log.Fatalf("MQTT client lost connection: %v", err)
+	m.disconnected = true
+	m.retryConnect()
+}
